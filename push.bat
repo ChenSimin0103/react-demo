00ba750d3e6e1a5efba798b529f9ca11719667d2
ChenSimin0103 <375021462@qq.com>
2019-03-18 23:10:13 +0800

    娣诲姞demo

--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "first commit"
-git remote add origin git@github.com:ChenSimin0103/react-demo.git
-git push -u origin master
+git commit -m "添加demo"
+REM git remote add origin git@github.com:ChenSimin0103/react-demo.git
+git push origin master
